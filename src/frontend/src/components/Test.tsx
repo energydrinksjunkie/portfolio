0e73016304770a30db83f8f7dd0d36954cc16c78
Dragan <90929387+energydrinksjunkie@users.noreply.github.com>
2024-12-23 15:23:17 +0100

    testing

--- src/frontend/src/components/Test.tsx
+++ src/frontend/src/components/Test.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Test: React.FC = () => {
   return (
-    <div id="html-content" style={{ width: '640px', height: '480px', backgroundColor: 'white', color: 'black', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid black' }}>
+    <div id="html-content" style={{ width: '640px', height: '480px', backgroundColor: 'white', color: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', alignContent: 'space-around'}}>
       <h1>Kiti Krvc</h1>
+      <p>meow meow meow meow meow</p>
+      <form>
+        <input type="text" placeholder="Name" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
