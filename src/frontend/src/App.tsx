714c1ce58e33111c8eb8e42c7ba918aa273713fe
Dragan <90929387+energydrinksjunkie@users.noreply.github.com>
2024-12-26 09:36:00 +0100

    added camera contraints

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
@@ -8,16 +7,24 @@ import Keyboard from './models/Keyboard'
 import Chair from './models/Chair'
 
 function App() {
+
   return (
     <Canvas gl={{ preserveDrawingBuffer: true }}
-    camera={{ position: [-0.3, 1.1, 0.7], fov: 75 }}
+    camera={{ position: [-0.3, 1.1, 0.8], fov: 75 }}
     >
       <ambientLight />
       <Monitor scale={.8} position={[-.3,0.815,0]} rotation={[0,0.3,0]}/>
       <Desk />
       <Keyboard rotation={[0,0.1,0]} position={[-.3,0.808,.32]}/>
       <Chair position={[-.3,0,.7]} rotation={[0,3,0]} />
-      <OrbitControls />
+      <OrbitControls 
+      target={[-0.3,1,0]} 
+      minPolarAngle={Math.PI/10} 
+      maxPolarAngle={Math.PI/2} 
+      minAzimuthAngle={-Math.PI/6} 
+      maxAzimuthAngle={Math.PI/6} 
+      enableZoom={false}
+      enableRotate={true}/>
     </Canvas>
   )
 }
